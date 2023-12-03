28bf1e0096c0b29033530ca31f6b13e038e586c2
David Saley <davidka907@gmail.com>
2023-12-04 01:13:42 +0300

    feat: init prisma

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+import { User } from '.prisma/client';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +10,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/create')
+  async createMonUser(): Promise<User> {
+    return this.appService.createMockUser();
+  }
+
+  @Get('/list')
+  async getUserList(): Promise<User[]> {
+    return this.appService.getUserList();
   }
 }
--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { prisma } from 'db';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createMockUser() {
+    return await prisma.user.create({
+      data: {
+        name: 'Dav',
+        email: 'test@gmail.com',
+      },
+    });
+  }
+
+  async getUserList() {
+    return await prisma.user.findMany({});
   }
 }
